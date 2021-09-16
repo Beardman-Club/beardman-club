ab275ab611a1fc55a321d52bbe51e58a74cc9578
soushi888 <sacha.pignot@protonmail.com>
2021-09-16 07:21:43 -0400

    error layout

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,9 +1,22 @@
-<h1>Error 404</h1>
+<div>
+	<h1>Error 404</h1>
+</div>
 
-<style>
-	h1 {
-			font-size: 4rem;
-			text-align: center;
-			margin: 100px auto;
-	}
+<style lang='scss'>
+  div {
+    height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      font-size: 4rem;
+      margin: 100px auto;
+      background: var(--color-primary);
+      color: var(--background-color__light);
+      padding: 100px 200px;
+      border-radius: 20px;
+      box-shadow: 9px 5px 15px 7px #343434;
+    }
+  }
 </style>
