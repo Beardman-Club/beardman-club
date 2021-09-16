cb1690c5d1565387c798f53a9aa0d232591928c3
soushi888 <sacha.pignot@protonmail.com>
2021-09-16 06:52:08 -0400

    ajout texte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,37 @@
 <script lang='ts'>
-	import Navbar from '../components/Navbar.svelte';
-	import BeardmansGrid from '../components/Beardman/BeardmansGrid.svelte';
-	import Footer from '../components/Footer.svelte';
+  import BeardmansGrid from '../components/Beardman/BeardmansGrid.svelte';
 </script>
 
 <main>
-	<BeardmansGrid />
+    <div class="text">
+        <h1>Welcome to the Beardman Club !</h1>
+        <p>
+            The Beardman Club is a club about the Mystic Beard. This is the beard that unites us against our enemies of
+            all time. A group of ten Beardman fighting for their life deep in the forest found a temple inside the dark
+            mountain. They found a old script talking about the mystical beard fighting the enemies
+        </p>
+    </div>
+
+    <BeardmansGrid/>
 </main>
 
-<style>
-    main {
-        margin: 25px auto;
+<style lang="scss">
+  main {
+    margin: 50px 80px;
+
+    .text {
+      margin-bottom: 50px;
+
+      h1 {
+        font-size: 3rem;
+        text-align: center;
+      }
+
+      p {
+        font-size: 1.5rem;
+        margin: 20px auto;
+        text-align: center;
+      }
     }
+  }
 </style>
