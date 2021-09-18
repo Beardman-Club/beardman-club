fc02158638623797015eec078a2e0848f09bd9fb
soushi888 <sacha.pignot@protonmail.com>
2021-09-18 05:36:54 -0400

    Création composante Social

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,18 +1,13 @@
 <script lang='ts'>
-	import twitterLogo from '/assets/images/twitter.png';
-	import discordLogo from '/assets/images/discord-logo-transparent.png';
+	import Social from './Social.svelte';
 
-	const twitterLink: string = 'https://twitter.com/BeardmanClub';
-	const discordLink: string = 'https://discord.gg/MjUv4cDt';
 	const currentYear = new Date().getFullYear();
 </script>
 
 <footer>
 	<p class='copyright'>©{currentYear} Beardmans Club | All right reserved.</p>
-	<div class='social'>
-		<a href='{twitterLink}' target='_blank'><img src='{twitterLogo}' alt='Twitter logo'></a>
-		<a href='{discordLink}' target='_blank'><img src='{discordLogo}' alt='Discord logo'></a>
-	</div>
+
+	<Social />
 </footer>
 
 <style lang='scss'>
@@ -24,28 +19,13 @@
     border-top: 3px black solid;
     padding: 40px var(--container-size);
 
-    p {
+    .copyright {
       font-size: 1.5rem;
     }
 
-    .social {
-      display: flex;
-      gap: 20px;
-
-      img {
-        width: 80px;
-
-        &:hover {
-          position: relative;
-          bottom: 5px;
-          cursor: pointer;
-        }
-      }
-    }
-
     @media (max-width: 600px) {
-			padding: 40px;
-			gap: 40px;
+      padding: 40px;
+      gap: 40px;
       flex-direction: column;
     }
   }
