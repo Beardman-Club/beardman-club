c56b03eed60a966250a76ea5ab35441a8ba15e1b
soushi888 <sacha.pignot@protonmail.com>
2021-09-20 15:36:10 -0400

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	adapter: adapter(),
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({}),
